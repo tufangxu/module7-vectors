a3a0c8d9f9b6113903a43095b0221c0a071610ef
Coco xu <tufanx@uw.edu>
2017-01-12 15:13:32 -0800

    Add bonus 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,24 +1,23 @@
 # Exercise 2: Subsetting and Manipulating Vectors
 
 # Create a vector `x` that contains the numbers 5,2,6,2,1,7
-
+x <- c(5,2,6,2,1,7)
 
 # Create a vector `y` that contains the numbers 2,3
-
+y <- c(2,3)
 
 # Create a vector `z` by adding (not combining, but adding) `x` and `y` (note the recycling!)
-
+z <- x+y
 
 # Create a vector `first.three` that has the first three elements of `z` in it
-
+first.three <- z[1:3]
 
 # Create a vector `small` that has the values of `z` that are less than 5
-
+small <- z[z<4]
 
 # Create a vector `large` that has the values of `z` that are greater than or equal to 5
-
-
-### Bonus ###
+large <- z[z>=5] 
+### Bonus ### 
 
 # Replace the values in `z` that are larger than 5 with the number 5
 
